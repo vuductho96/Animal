0477f7de5d4c5188c67be6328b865204093c2575
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-04-19 16:43:47 +0700

    Animal

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Animal
+{
+    public abstract class Animal
+    {
+
+        public string Breed { get; set; }
+        public int age { get; set; }
+
+        public  Animal(string breed, int age)
+        {
+           Breed = breed;
+            this.age = age;
+        }
+        public abstract void PrintInfo();
+    }
+   
+
+    public class Cat : Animal
+    {
+       public string name { get; set; }
+       
+        public Cat(string breed, int age, string name): base (breed,age)
+        {
+            this.name = name;
+        }
+        public override void PrintInfo()
+        {
+            Console.WriteLine("Weight : {0}" + '\n' + "Height: {1}" + '\n' + "Name: {2}", this.Breed, this.age, this.name);
+        }
+
+    }
+
+    public class Program
+    {
+        static void Main (string []args)
+        {
+            Cat cat1 = new Cat("Dog",20,"Meo con");
+            cat1.PrintInfo();
+
+
+        }
+
+    }
+}
